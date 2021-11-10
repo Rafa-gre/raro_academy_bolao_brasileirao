4d831041033f6cd8ab44c1b0361536bcf8e9f38c
rafa-gre <rafael@gregorini.com.br>
2021-11-10 17:27:36 -0300

    Fix CampeonatoService

--- src/service/CampeonatoService.ts
+++ src/service/CampeonatoService.ts
@@ -51,54 +51,6 @@ export class CampeonatoService implements ICampeonatoService {
             throw error;
         }
     }
-    private async dadosRodada(idCampeonatoApiExterna: number) {
-
-    }
-
-
-    private campeonatoFactory(idCampeonatoApi: number, dadosCampeonato: CampeonatoDTO): Campeonato {
-        const campeonato = new Campeonato();
-
-        campeonato.id = dadosCampeonato.id;
-        campeonato.nome = dadosCampeonato.nome;
-        campeonato.slug = dadosCampeonato.slug;
-        campeonato.idCampeonatoApiExterna = idCampeonatoApi;
-
-        return campeonato;
-    }
-
-    private rodadaFactory = (
-        dadosRodada: RodadaDetalhe,
-    ): Rodada => {
-        const numeroRodada = dadosRodada.rodada;
-        const partidas: Partida[] = dadosRodada.partidas.map((partida) => {
-            const mandante = new Time();
-            partida.time_mandante.time_id = mandante.id;
-            partida.time_mandante.nome_popular = mandante.nome;
-            partida.time_mandante.sigla = mandante.sigla;
-            partida.time_mandante.escudo = mandante.escudo;
-
-            const visitante = new Time()
-            partida.time_visitante.time_id = visitante.id;
-            partida.time_visitante.nome_popular = visitante.nome;
-            partida.time_visitante.sigla = visitante.sigla;
-            partida.time_visitante.escudo = visitante.escudo;
-
-            const data = new Date(partida.data_realizacao_iso);
-            const id = partida.partida_id;
-            const jogo = new Partida();
-            jogo.mandante = mandante;
-            jogo.visitante = visitante;
-            jogo.dataRealizacao = data
-            jogo.id = id;
-            return jogo;
-        });
-        const rodada = new Rodada();
-        rodada.rodada = numeroRodada;
-        rodada.partida = partidas;
-        return rodada
-
-    };
 
 
 }
